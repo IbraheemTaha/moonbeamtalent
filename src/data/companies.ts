228e16f633c7b29b319888a2372c4e0a0d67ac9b
ibraheemtaha <ibraheemtaha@yahoo.com>
2026-04-21 20:25:09 +0200

    adding viebke and west logo

--- src/data/companies.ts
+++ src/data/companies.ts
@@ -1,6 +1,7 @@
 export const companies = [
   { name: "Onomondo", logo: "/img/logos/onomondo.png" },
   { name: "Curity", logo: "/img/logos/curity.svg" },
+  { name: "Viebke", logo: "/img/logos/viebke_west_vertical.svg" },
   { name: "Neurons", logo: "/img/logos/neurons.png" },
   { name: "OYO", logo: "/img/logos/OYO-logo.svg" },
   { name: "Mesame", logo: "/img/logos/Mesame.webp" },
